adfa8273864f588f21d61f1120939eda669e4fff
Мальцева Наталья Витальевна <n.maltseva2@main.nsu.ru>
2025-02-01 14:10:48 +0700

    Доделала третью задачу

--- mineswipper.py
+++ mineswipper.py
@@ -23,26 +23,88 @@ m = [
     [],
     []
 ]
+z = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
 for i in range(10):
     for j in range(10):
         if(i % 2 == 1 and j % 2 == 1) or (i % 2 == 0 and j % 2 == 0):
             m[i].append(0)
+            z[i].append(0)
         else:
-            m[i].append(0.00001)
+            m[i].append(1)
+            z[i].append(0)
+#for i in range(10):
+#    z[0].append(0)
+#    z[11].append(0)
+#    z[i].append(0)
+#    z[i].append(0)
 
 fig = plt.figure(figsize=(10, 6))
 ax = plt.subplot(1, 2, 1)
-plt.imshow(m, cmap="magma")
+plt.imshow(m, cmap="Pastel1")
 
 i = 0
 while(i < 15):
     x = randint(0, 9)
     y = randint(0, 9)
-    if m[x][y] == 0 or m[x][y] == 0.00001:
+    if m[x][y] == 0 or m[x][y] == 1:
         m[x][y] = -1
-        cil = plt.Circle((x, y), 0.35, color="#5d66a3")
+        z[y][x] = 10
+        if(y > 0 and y < 9 and z[y-1][x] != 10):
+            z[y-1][x] += 1
+        if(y > 0 and y < 9 and z[y + 1][x] != 10):
+            z[y+1][x] += 1
+        if(y == 0 and z[y+1][x] != 10):
+            z[y+1][x] += 1
+        elif(y == 9 and z[y-1][x] != 10):
+            z[y-1][x] += 1
+
+        if (x > 0 and x < 9 and z[y][x-1] != 10):
+            z[y][x-1] += 1
+        if (x > 0 and x < 9 and z[y][x+1] != 10):
+            z[y][x+1] += 1
+        if (x == 0 and z[y][x+1] != 10):
+            z[y][x+1] += 1
+        elif (x == 9 and z[y][x-1] != 10):
+            z[y][x-1] += 1
+
+        if(x > 0 and y > 0 and z[y-1][x-1] != 10):
+            z[y-1][x-1] += 1
+        if(x <  9 and y < 9 and z[y+1][x+1] != 10):
+            z[y+1][x+1] += 1
+        if(y > 0 and x < 9 and z[y-1][x+1] != 10):
+            z[y-1][x+1] += 1
+        if (x > 0 and y < 9 and z[y + 1][x - 1] != 10):
+            z[y + 1][x - 1] += 1
+        cil = plt.Circle((x, y), 0.35, color="#c41414")
         ax.add_patch(cil)
         i += 1
+for i in z:
+    print(i)
+plt.xticks(range(10), labels=[f"{i}" for i in range(1, 11)])
+plt.yticks(range(10), labels=[f"{i}" for i in range(10, 0, -1)])
 
+ax = plt.subplot(1, 2, 2)
+
+plt.imshow(z, cmap="OrRd")
+
+for i in range(10):
+    for j in range(10):
+        if(z[i][j] == 10):
+            plt.text(j, i, "M")
+        else:
+            plt.text(j, i, z[i][j])
 
 plt.show()
+
+#plt.text(j, i, "M")
